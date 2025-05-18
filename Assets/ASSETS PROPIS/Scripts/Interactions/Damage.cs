191a446811f6fcb2dc21daca7b3073a283030763
Daniel <dperezterron19@gmail.com>
2025-05-18 20:02:28 +0200

    Arreglau

--- Assets/ASSETS PROPIS/Scripts/Interactions/Damage.cs
+++ Assets/ASSETS PROPIS/Scripts/Interactions/Damage.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
-    public int swordSpeed;
+    //public int swordSpeed;
     public Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        swordSpeed=Random.Range(1, 10);
+        //swordSpeed=Random.Range(1, 10);
         rb=GetComponent<Rigidbody>();
-        rb.velocity=transform.right * swordSpeed;
+        //rb.velocity=transform.right * swordSpeed;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Le ha dado");
+        
 
         if (other.CompareTag("Player"))
         {
             var health = other.GetComponent<Health>();
             if (health != null) health.TakeDamage(1);
+            Debug.Log("Le ha dado");
         }
 
         // Empalmar el objeto a lo que colisiona (si no es la espada)
