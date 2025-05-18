a67d8d467153a0a1a0f3712c917c7a0793635170
nora-pastoret <nora.pastoret@estudiantat.upc.edu>
2025-05-18 17:02:17 +0200

    wip grab esqueleto

---
+++ Assets/ASSETS PROPIS/Scripts/GrabEsqueleto.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class GrabEsqueleto : MonoBehaviour
+{
+    private XRGrabInteractable grab;
+    private ConfigurableJoint joint;
+    private bool grabEnabled = false;
+
+    void Start()
+    {
+        grab = GetComponent<XRGrabInteractable>();
+        joint = GetComponent<ConfigurableJoint>();
+
+        if (grab != null)
+            grab.enabled = false; // Ens assegurem que comenci desactivat
+    }
+
+    void Update()
+    {
+        if (!grabEnabled)
+        {
+            if (joint == null)
+            {
+                // Potser s'ha destruït a runtime
+                grab = GetComponent<XRGrabInteractable>();
+                if (grab != null)
+                {
+                    grab.enabled = true;
+                    grabEnabled = true;
+                }
+            }
+            else if (!joint.connectedBody)
+            {
+                // Opcional: si el joint encara existeix però s'ha "trencat" (sense connexió)
+                Destroy(joint); // Opcional: el treus i deixes que s'activi el grab a la propera iteració
+            }
+        }
+    }
+}
