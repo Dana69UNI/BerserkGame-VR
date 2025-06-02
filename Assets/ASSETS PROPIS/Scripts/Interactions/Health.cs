f6a28e6a4a0ebfb3140b92294b9508c7cfb023c5
anastasiaanaya <154979891+anastasiaanaya@users.noreply.github.com>
2025-06-02 22:49:11 +0200

    deathAnimation

--- Assets/ASSETS PROPIS/Scripts/Interactions/Health.cs
+++ Assets/ASSETS PROPIS/Scripts/Interactions/Health.cs
@@ -2,29 +2,69 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
-    public int maxHealth = 60;
+    public int maxHealth = 5;
     public int currentHealth;
+
+    public Image damageImage;
+
+    [Range(0f, 1f)]
+    public float[] damageAlphas;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+
+        if (damageImage == null)
+        {
+            Debug.LogError("Fade Image is not assigned in the FadeController.");
+        }
+
+        else
+        {
+            //Ens assegurem que comenci sent transparent la imatge
+            var c = damageImage.color;
+            c.a = 0f;
+            damageImage.color = c;
+        }
+
+        if(damageAlphas == null || damageAlphas.Length > maxHealth)
+        {
+            Debug.LogError("Damage alphas array is not assigned or empty.");
+            
+        } 
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
 
-        if(currentHealth <= 0)
+        int hitsTaken = maxHealth - currentHealth;
+
+        if(damageImage != null && damageAlphas != null && hitsTaken >0 && hitsTaken <= damageAlphas.Length)
+        {
+            float newAlpha = damageAlphas[hitsTaken - 1];
+            SetImageAlpha(newAlpha);
+        }
+
+        if (currentHealth <= 0)
         {
             //Estarem morts
             currentHealth = 0;
             Die();
         }
+    }
+
+    private void SetImageAlpha(float alpha)
+    {
+        var c = damageImage.color;
+        c.a = alpha;
+        damageImage.color = c;
     }
 
     void Heal(int amount)
